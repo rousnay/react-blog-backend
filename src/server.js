daf3ad0bfd121491c5b6894770ccb992aa049739
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-11-24 05:54:39 +0600

    POST method added for the comment

--- src/server.js
+++ src/server.js
@@ -1,17 +1,13 @@
 import express from "express";
 
 let articlesInfo = [
-  { name: "learn-react", upvotes: 0 },
-  { name: "learn-node", upvotes: 0 },
-  { name: "mongodb", upvotes: 0 },
+  { name: "learn-react", upvotes: 0, comments: [] },
+  { name: "learn-node", upvotes: 0, comments: [] },
+  { name: "mongodb", upvotes: 0, comments: [] },
 ];
 
 const app = express();
 app.use(express.json());
-
-app.post("/hello", (req, res) => {
-  res.send(req.body.name);
-});
 
 app.get("/hello/fName/:fName/lName/:lName", (req, res) => {
   const { fName, lName } = req.params;
@@ -20,16 +16,29 @@ app.get("/hello/fName/:fName/lName/:lName", (req, res) => {
 
 app.put("/api/articles/:name/upvotes", (req, res) => {
   const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
 
-  const articles = articlesInfo.find((article) => article.name === name);
-
-  if (articles) {
+  if (article) {
     articles.upvotes += 1;
     res.send(
-      `The ${articles.name} article now has ${articles.upvotes} upvotes`
+      `The ${article.name} article now has ${article.upvotes} upvotes!!`
     );
   } else {
     res.send(`The article dosn\'t exist!`);
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((a) => a.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("The article dosn't exist!");
   }
 });
 
