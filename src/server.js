437cf33f1925cb4aa841cf823856feb211e630d0
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-11-24 04:42:04 +0600

    PUT method added for up voting a article

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 
+let articlesInfo = [
+  { name: "learn-react", upvotes: 0 },
+  { name: "learn-node", upvotes: 0 },
+  { name: "mongodb", upvotes: 0 },
+];
+
 const app = express();
 app.use(express.json());
 
@@ -10,6 +16,21 @@ app.post("/hello", (req, res) => {
 app.get("/hello/fName/:fName/lName/:lName", (req, res) => {
   const { fName, lName } = req.params;
   res.send(`Hello ${(fName, lName)}!`);
+});
+
+app.put("/api/articles/:name/upvotes", (req, res) => {
+  const { name } = req.params;
+
+  const articles = articlesInfo.find((article) => article.name === name);
+
+  if (articles) {
+    articles.upvotes += 1;
+    res.send(
+      `The ${articles.name} article now has ${articles.upvotes} upvotes`
+    );
+  } else {
+    res.send(`The article dosn\'t exist!`);
+  }
 });
 
 app.listen(8000, () => {
