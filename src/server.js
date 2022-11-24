5c288a95ec60f8dbd28ae7873f96325af047287b
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-11-24 07:24:41 +0600

    Mongodb implemented for get method

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
-
-let articlesInfo = [
-  { name: "learn-react", upvotes: 0, comments: [] },
-  { name: "learn-node", upvotes: 0, comments: [] },
-  { name: "mongodb", upvotes: 0, comments: [] },
-];
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
 
-app.get("/hello/fName/:fName/lName/:lName", (req, res) => {
-  const { fName, lName } = req.params;
-  res.send(`Hello ${(fName, lName)}!`);
+app.get("/api/articles/:name/comments", async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db("react-blog-db");
+  const articles = db.collection("articles");
+  const articleByName = await articles.findOne({ name });
+  // res.send(articles).json();
+  res.json(articleByName);
 });
 
 app.put("/api/articles/:name/upvotes", (req, res) => {
