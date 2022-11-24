bab81bdec0e5f9413a82b3cf5f1cab7680cd4009
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-11-24 17:58:53 +0600

    PUT method added with database

--- src/server.js
+++ src/server.js
@@ -12,18 +12,27 @@ app.get("/api/articles/:name/comments", async (req, res) => {
   const db = client.db("react-blog-db");
   const articles = db.collection("articles");
   const articleByName = await articles.findOne({ name });
-  // res.send(articles).json();
-  res.json(articleByName);
+
+  if (articleByName) {
+    res.json(articleByName);
+  } else {
+    res.send(`The article dosn\'t exist!`);
+  }
 });
 
-app.put("/api/articles/:name/upvotes", (req, res) => {
+app.put("/api/articles/:name/upvotes", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((a) => a.name === name);
 
-  if (article) {
-    articles.upvotes += 1;
-    res.send(
-      `The ${article.name} article now has ${article.upvotes} upvotes!!`
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db("react-blog-db");
+  const articles = db.collection("articles");
+  const articleByName = await articles.findOne({ name });
+
+  if (articleByName) {
+    await articles.updateOne({ name }, { $inc: { upvotes: 1 } });
+    res.json(
+      `The ${name} article now has ${articleByName.upvotes + 1} upvotes!!`
     );
   } else {
     res.send(`The article dosn\'t exist!`);
